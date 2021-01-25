c7476aa26b285958c2694616e5bd76c6848ec428
zaidzeya <zaidzeya@mail.ru>
2021-01-25 19:55:08 +0300

    переделала как в примере про PingPong + добавила logger вместо sout

--- threadPrinter/src/main/java/lesson_threadPrinter/printThreads.java
+++ threadPrinter/src/main/java/lesson_threadPrinter/printThreads.java
@@ -1,32 +1,50 @@
 package lesson_threadPrinter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.TimeUnit;
 
 public class printThreads implements Runnable{
-	public static void main(String[] args) {}
+	private static final Logger logger = LoggerFactory.getLogger(printThreads.class);
+	private final String str = "1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1";
+	private String last = "Thread 2";
 
 	@Override
 	synchronized public void run() {
-		String str = "1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1";
 		String[] words = str.split(" ");
 
 		for (String word : words) {
-			if (Thread.currentThread().getName().equals("Thread #1")) {
-				System.out.println("Поток '"+Thread.currentThread().getName()+"': "+word);
-			} else {
-				System.out.println("Поток '"+Thread.currentThread().getName()+"': "+"  " + word);
-			}
 			try {
-				notify();
-				Thread.sleep( TimeUnit.MILLISECONDS.toMillis( 250 ) );
-				wait();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				if (last.equals(Thread.currentThread().getName())) {
+					this.wait();
+				}
+				logger.info(word);
+				last = Thread.currentThread().getName();
+				sleep();
+				notifyAll();
+			} catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+				throw new NotInterestingException(ex);
 			}
 		}
-
-		Thread.currentThread().interrupt();
-		notifyAll();
 	}
+
+	private static void sleep() {
+		try {
+			Thread.sleep(1_000);
+		} catch (InterruptedException e) {
+			logger.error(e.getMessage());
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	private static class NotInterestingException extends RuntimeException {
+		NotInterestingException(InterruptedException ex) {
+			super(ex);
+		}
+	}
+
+	public static void main(String[] args) {}
 }
 
